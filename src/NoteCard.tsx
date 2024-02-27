c538a619dbd456b272ec964bfdaf16f9cfce8a26
Dmitry <dmitry27zh@gmail.com>
2024-02-27 13:04:24 +0500

    Create NoteCard

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -1,9 +1,27 @@
+import { Badge, Card, Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { Note } from './App'
+import styles from './NoteList.module.css'
 
 type SimplifiedNote = Pick<Note, 'id' | 'title' | 'tags'>
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
-  return <p>Card</p>
+  return (
+    <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+      <Card.Body>
+        <Stack gap={2} className="align-items-center justify-content-center h-100">
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack direction="horizontal" className="justify-content-center flex-wrap">
+              {tags.map((tag) => (
+                <Badge key={tag.id}>{tag.label}</Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default NoteCard
